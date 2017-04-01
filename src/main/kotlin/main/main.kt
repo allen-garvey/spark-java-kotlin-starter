26a82c7bc9e70c347c16821cc6c2f89995dcd620
Allen Garvey <allen.garvey@gmail.com>
2017-04-01 12:19:03 -0400

    Edit before routes to match new api and add filters to match routes with trailing slash

--- src/main/kotlin/main/main.kt
+++ src/main/kotlin/main/main.kt
@@ -15,10 +15,30 @@ fun main(args : Array<String>) {
 
     staticFiles.location("/public");
 
+    //allow routes to match with trailing slash
+    before(Filter({ req, res ->
+        val path = req.pathInfo()
+        if (!path.equals("/") && path.endsWith("/")){
+            res.redirect(path.substring(0, path.length - 1))
+        }
+    }))
+
+    //set response type to json for api routes
+    after(Filter({req, res ->
+        if(req.pathInfo().startsWith("/api")){
+            res.type("application/json")
+        }
+    }))
+
     //gzip everything
-    after({req, res ->
+    after(Filter({req, res ->
         res.header("Content-Encoding", "gzip")
-    })
+    }))
+
+    //used to parse and convert JSON
+    val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
 
     //used to parse and convert JSON
     val gson = Gson()
@@ -26,5 +46,5 @@ fun main(args : Array<String>) {
 
     get("/", { req, res -> ModelAndView(hashMapOf(Pair("name", "Test")), "index.hbs")  }, templateEngine)
     get("/hello/:name", { req, res -> ModelAndView(hashMapOf(Pair("name", req.params(":name"))), "index.hbs")  }, templateEngine)
-    get("/user/:first/:last/json", { req, res -> User(req.params(":first"), req.params(":last")) }, { gson.toJson(it) })
+    get("api/user/:first/:last", { req, res -> User(req.params(":first"), req.params(":last")) }, { gson.toJson(it) })
 }
